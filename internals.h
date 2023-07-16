03cdb42e486af31b85d74c6b211056282370622e
Bernie Lim <limbernie@users.noreply.github.com>
2023-07-16 07:34:40 +0000

    Convert CRLF to LF and tab to space

--- internals.h
+++ internals.h
@@ -3,56 +3,56 @@
 
 typedef struct _UNICODE_STRING 
 {
-	USHORT Length;
-	USHORT MaximumLength;
-	PWCH   Buffer;
+  USHORT Length;
+  USHORT MaximumLength;
+  PWCH   Buffer;
 } UNICODE_STRING;
 
 typedef struct _PEB_LDR_DATA 
 {
-	ULONG      Length;
-	BOOLEAN    Initialized;
-	PVOID      SsHandle;
-	LIST_ENTRY InLoadOrderModuleList;
-	LIST_ENTRY InMemoryOrderModuleList;
-	LIST_ENTRY InInitializationOrderModuleList;
+  ULONG      Length;
+  BOOLEAN    Initialized;
+  PVOID      SsHandle;
+  LIST_ENTRY InLoadOrderModuleList;
+  LIST_ENTRY InMemoryOrderModuleList;
+  LIST_ENTRY InInitializationOrderModuleList;
 } PEB_LDR_DATA, * PPEB_LDR_DATA;
 
 typedef struct _PEB 
 {
-	BYTE          Reserved[12];
-	PEB_LDR_DATA* Ldr;
+  BYTE          Reserved[12];
+  PEB_LDR_DATA* Ldr;
 } PEB, * PPEB;
 
 typedef struct _LDR_MODULE
 {
-	LIST_ENTRY      InLoadOrderModuleList;
-	LIST_ENTRY      InMemoryOrderModuleList;
-	LIST_ENTRY      InInitializationOrderModuleList;
-	PVOID           BaseAddress;
-	PVOID           EntryPoint;
-	ULONG           SizeOfImage;
-	UNICODE_STRING  FullDllName;
-	UNICODE_STRING  BaseDllName;
-	ULONG           Flags;
-	SHORT           LoadCount;
-	SHORT           TlsIndex;
-	LIST_ENTRY      HashTableEntry;
-	ULONG           TimeDateStamp;
+  LIST_ENTRY      InLoadOrderModuleList;
+  LIST_ENTRY      InMemoryOrderModuleList;
+  LIST_ENTRY      InInitializationOrderModuleList;
+  PVOID           BaseAddress;
+  PVOID           EntryPoint;
+  ULONG           SizeOfImage;
+  UNICODE_STRING  FullDllName;
+  UNICODE_STRING  BaseDllName;
+  ULONG           Flags;
+  SHORT           LoadCount;
+  SHORT           TlsIndex;
+  LIST_ENTRY      HashTableEntry;
+  ULONG           TimeDateStamp;
 } LDR_MODULE, * PLDR_MODULE;
 
 typedef struct _CLIENT_ID
 {
-	PVOID UniqueProcess;
-	PVOID UniqueThread;
+  PVOID UniqueProcess;
+  PVOID UniqueThread;
 } CLIENT_ID, * PCLIENT_ID;
 
 typedef struct _TEB
 {
-	NT_TIB    NtTib;
-	PVOID     EnvironmentPointer;
-	CLIENT_ID ClientId;
-	PVOID     ActiveRpcHandle;
-	PVOID     ThreadLocalStoragePointer;
-	PPEB      ProcessEnvironmentBlock;
+  NT_TIB    NtTib;
+  PVOID     EnvironmentPointer;
+  CLIENT_ID ClientId;
+  PVOID     ActiveRpcHandle;
+  PVOID     ThreadLocalStoragePointer;
+  PPEB      ProcessEnvironmentBlock;
 } TEB, * PTEB;
